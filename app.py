c77d3d0230016d114e86c457b5ca8d21f486dfab
L00163434 <91318384+Snehalkhairnar9@users.noreply.github.com>
2022-08-26 16:07:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app.py code is shown here. This is a functionality to test the sphinx for auto d
 
 app = Flask(__name__)
 mysql = MySQL(app)
-app.config['MYSQL_HOST'] = 'database-1.c3szodjsmjlg.us-east-2.rds.amazonaws.com'
-app.config['MYSQL_USER'] = 'admin'
-app.config['MYSQL_PASSWORD'] = 'Merchant#1'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Snehal@9'
 app.config['MYSQL_DB'] = 'customer'
 app.config['MYSQL_PORT'] = 3306
 
